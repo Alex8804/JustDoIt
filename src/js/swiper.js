c9e46555984b3cb546fcb7aede4243eafd97236c
vosob <osobskyi.vlad@gmail.com>
2023-06-03 02:11:20 +0300

    slider done

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,31 +1,31 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true, // что-бы сделать пагинацию кликабельной
-  },
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true, // что-бы сделать пагинацию кликабельной
+//   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
 
 // Swiper for reviews
-var reviewsSwiper = new Swiper('.slide-container', {
+const reviewsSwiper = new Swiper('.slide-container', {
   slidesPerView: 1,
   // slidesPerGroup: 2,
-  spaceBetween: 20,
+  spaceBetween: 28,
   // centerInsufficientSlides: true,
   // centeredSlides: true,
   // centeredSlidesBounds: true,
@@ -34,9 +34,9 @@ var reviewsSwiper = new Swiper('.slide-container', {
       slidesPerView: 2,
       spaceBetween: 16,
     },
-    1024: {
+    1200: {
       slidesPerView: 3,
-      spaceBetween: 26,
+      spaceBetween: 28,
     },
   },
   pagination: {
