61804dd212d870c454e88a23d788b3972e4ef61d
vosob <osobskyi.vlad@gmail.com>
2023-06-02 23:34:36 +0300

    first commit

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,22 +1,46 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true, // что-бы сделать пагинацию кликабельной
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // что-бы сделать пагинацию кликабельной
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// Swiper for reviews
+var reviewsSwiper = new Swiper('.slide-container', {
+  slidesPerView: 1,
+  // slidesPerGroup: 2,
+  spaceBetween: 20,
+  // centerInsufficientSlides: true,
+  // centeredSlides: true,
+  // centeredSlidesBounds: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 26,
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
