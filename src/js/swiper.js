b20fc428020292704fa96ab793b676d662ada470
Ap01lY0n <128406680+Ap01lY0n@users.noreply.github.com>
2023-06-04 00:00:23 +0300

    1

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -44,3 +44,26 @@ const reviewsSwiper = new Swiper('.slide-container', {
     clickable: true,
   },
 });
+var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 28,
+        loop: false,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 18,
+            },
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 18,
+            },
+        },
+    });
